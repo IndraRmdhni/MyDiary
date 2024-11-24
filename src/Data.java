fec1624dd46cf64f7cbe7a5a245b6400e867a808
IndraRmdhni <fmyfuture1@gmail.com>
2024-11-24 15:21:58 +0800

    Perubahan Tampilan dan Penambahan Source Code

---
+++ src/Data.java
@@ -0,0 +1,26 @@
+public class Data {
+    private String judul;
+    private String isi;
+
+    public Data(String judul, String isi) {
+        this.judul = judul;
+        this.isi = isi;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public String getIsi() {
+        return isi;
+    }
+
+    public void setIsi(String isi) {
+        this.isi = isi;
+    }
+
+    @Override
+    public String toString() {
+        return judul; // Agar judul tampil di JList
+    }
+}
